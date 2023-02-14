d083367bc6fd1f179128ccc4c0ce6c29135d0d74
Nitin Singh <i.nitinsingh18@gmail.com>
2023-02-15 00:44:10 +0530

    Bread-Crumb instances

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +13,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter_provider',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
     );
+  }
+}
+
+class BreadCrumb {
+  bool isActive;
+  final String name;
+  final String uuid;
+
+  BreadCrumb({required this.isActive, required this.name}) : uuid = Uuid().v4();
+
+  void activate() {
+    isActive = true;
+  }
+
+  @override
+  bool operator ==(covariant BreadCrumb other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  String get title => name + (isActive ? " > " : '');
+}
+
+class BreadCrumbProvider extends ChangeNotifier {
+  final List<BreadCrumb> _items = [];
+  UnmodifiableListView<BreadCrumb> get item => UnmodifiableListView(_items);
+
+  void addBreadCrumb(BreadCrumb breadCrumb) {
+    for (final item in _items) {
+      item.activate();
+    }
+    _items.add(breadCrumb);
+    notifyListeners();
+  }
+
+  void removeBreadCrumb() {
+    _items.clear();
+    notifyListeners();
   }
 }
 
@@ -25,7 +66,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HomePage")),
+      appBar: AppBar(title: const Text("HomePage")),
     );
   }
 }
